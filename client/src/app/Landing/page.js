2e622c1d31e08d99e52e75779eb7922f85945616
Sai Rithwik Bejadi <rithwikbejadi696@gmail.com>
2025-07-29 10:09:38 +0530

    ..

--- client/src/app/Landing/page.js
+++ client/src/app/Landing/page.js
@@ -3,7 +3,7 @@
 export default function LandingPage() {
   return (
     <div className="bg-[url('/images/homepg_bg.png')] bg-cover bg-center bg-no-repeat h-screen m-0 p-0">
-        
+        hi
     </div>
   );
 }
